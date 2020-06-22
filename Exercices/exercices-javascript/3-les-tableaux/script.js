a1f129edf15c6daed8eb0971c3e3e4b8810ecd00
Joéllie Sprumont <joellie@siriusschool.be>
2020-06-22 14:48:39 +0200

    update

--- Exercices/exercices-javascript/3-les-tableaux/script.js
+++ Exercices/exercices-javascript/3-les-tableaux/script.js
@@ -1,6 +1,6 @@
 // Exercice 1: ajoute "Ted" au tableau suivant
 
-let castArray= ["Robin", "Barney", "Lily", "Marshal"];
+let castArray = ['Robin', 'Barney', 'Lily', 'Marshal'];
 
 // Exercice 2: Affiche l'index de "Ted" dans le tableau dans la console
 
@@ -14,10 +14,11 @@ let castArray= ["Robin", "Barney", "Lily", "Marshal"];
 
 // Exercice 7: Affiche le nouvel index de Ted et Robin dans le tableau dans la console
 
-// Exercice 8: à l'aide d'une boucle, affiche chaque élément du tableau dans la console
+// Exercice 8: à l'aide d'une boucle (forEach, console.log element), affiche chaque élément du tableau dans la console
 
 // Exercice 9: à l'aide de la méthode .findIndex(), trouve l'index de Lily, ensuite, affiche Lily dans la console
 
 // Exercice 10: à l'aide d'une boucle, multiplie par 2 chaque élément du tableau suivant, crée un nouveau tableau à l'aide de la méthode .push(), affiche le nouveau tableau dans la console
+//faire un forEach et push
 
 let multiplicationArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
--- Exercices/exercices-javascript/4-les-boucles/script.js
+++ Exercices/exercices-javascript/4-les-boucles/script.js
@@ -1,5 +1,9 @@
 // Exercice 1: A l'aide d'une boucle, affiche les chiffres de 0 à 100, place les chiffres dans un tableau que tu nommeras fizzBuzzArray, à l'aide de la méthode .push()
+// Boucle for de 0 à 100 qui s'auto incremente de 1 qui se pousse dans le tableau
 
 // Exercice 2: Toujours à l'aide d'une boucle, passe sur chaque élément du tableau que tu viens de créer (fizzBuzzArray), si l'élément est divisible par trois affiche "fizz" dans la console, si l'élément est divisible par cinq affiche "buzz", si il est divisible par trois et par cinq affiche "fizzBuzz"
+//boucle foreach
 
 // Exercice 3: A l'aide de l'objet Date (voir: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date) affiche tous les vendredi 13 de l'année 2020
+// exercice bonus
+// 1 boucle pour passer chaque mois de l'année, et une condition if else si vendredi = 13, console.log
